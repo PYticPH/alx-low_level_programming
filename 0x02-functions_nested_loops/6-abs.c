b7e67c96d96b7d73f541227ed97fc2492222ba79
PYticPH <pyticph@gmail.com>
2022-09-16 16:07:18 -0700

    Solved: task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - Function to print absolute  numbers
+ *
+ * @a: varaible/parameter
+ *
+ * Result: 0
+ */
+
+int _abs(int)
+{
+	if (a >= 0)
+		return (a);
+	else
+		return (-a);
+}
