bbd03456f156a634195ded4091c05c0fbf58b872
PYticPH <pyticph@gmail.com>
2022-09-17 11:35:56 -0700

    Fix: incorrect output in task 8 #2

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,25 +9,17 @@ void jack_bauer(void)
 {
 	int a;
 	int b;
-	int c;
-	int d;
 
-	for (a = 0; a <= 2; a++)
+	for (a = 0; a <= 23; a++)
 	{
-		for (b = 0; b <= 3; b++)
+		for (b = 0; b <= 59; b++)
 		{
-			for (c = 0; c <= 5; c++)
-			{
-				for (d = 0; d <= 9; d++)
-				{
-					_putchar(a + '0');
-					_putchar(b + '0');
-					_putchar(':');
-					_putchar(c + '0');
-					_putchar(d + '0');
-					_putchar('\n');
-				}
-			}
+			_putchar((a / 10) + '0');
+			_putchar((a % 10) + '0');
+			_putchar(':');
+			_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
