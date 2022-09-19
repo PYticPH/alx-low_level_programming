729d6300130bd0af153e3183046f0a212ac9c518
PYticPH <pyticph@gmail.com>
2022-09-19 09:14:00 -0700

    Solved: task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * reset_to_98 - fuction that takes a pointer parameter
+ *
+ * description: update the value pointed
+ *
+ * @n: parameter
+ *
+ * return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+/**
+ * Filetype - header file
+ */
+
+int _putchar(void);
+void reset_to_98(int *n);
