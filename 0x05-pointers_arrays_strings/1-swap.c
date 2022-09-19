ccbef8542f82977f3a9c0f981847c93744e48c99
PYticPH <pyticph@gmail.com>
2022-09-19 10:18:57 -0700

    Solved: task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int -function to swap the value of two integers
+ *
+ * @a: parameter 1
+ * @b: parameter 2
+ *
+ * return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
