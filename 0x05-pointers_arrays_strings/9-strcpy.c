eb1b980c787ed201423aeca377e1ae6a842706f9
PYticPH <pyticph@gmail.com>
2022-09-22 01:03:38 -0700

    Solved: task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * _strcpy - function to copy strings
+ *
+ * @dest: parameter
+ * @src: parameter
+ *
+ * Return: char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int lpn, x;
+	
+	lpn = _strlen(src);
+
+	for (x = 0; x <= lpn; x++)
+	{
+		*(dest + x) = src[x];
+	}
+	return (dest);
+}
+
+/**
+ * _strlen - function to count the lenght of a string
+ *
+ * @s: parameter
+ *
+ * Return: 0
+ */
+
+
+
+int _strlen(char *s)
+{
+	int x;
+	int count = 0;
+
+	for (x = 0; s[x] != '\0'; x++)
+		count += 1;
+	return count;
+}
