0dc836a98762ca3c3e85f09a6c1a30c87a685cb0
PYticPH <pyticph@gmail.com>
2022-09-23 06:24:47 -0700

    Fix: task 1 incorrect output

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,6 @@
 #include "main.h"
+
+int _strlen(char *c);
 /**
  * _strncat - fuction to concatenate string
  *
@@ -12,22 +14,31 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	char *sptr;
-	int x, y;
+	int x, len_dest;
 
-	for (x = 0; x > -1; x++)
-	{
-		if (dest[x] == '\0')
-		{
-			for (y = 0; y < n; y++)
-			{
-				*(dest + x) = src[y];
-				x++;
-			}
-			break;
-		}
-		else
-			continue;
-	}
+	len_dest = _strlen(dest);
+	for (x = 0; x < n && src[x] != '\0'; x++)
+		dest[len_dest + x] = src[x];
+	dest[len_dest + 1 + x] = '\0';
 	sptr = dest;
 	return (sptr);
+}
+
+
+/**
+ * _strlen - fuction to return length of string
+ *
+ * @c: parameter
+ *
+ * Return: length of src
+ */
+
+int _strlen(char *c)
+{
+	int x;
+	int count = 0;
+
+	for (x = 0; c[x] != '\0'; x++)
+		count += 1;
+	return (count);
 }
