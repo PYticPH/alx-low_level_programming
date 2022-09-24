92cbd6d5e48ef5546e9a0c43a043c5bd741ef3bf
PYticPH <pyticph@gmail.com>
2022-09-24 08:49:02 -0700

    Fix: incorrect output in task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -17,8 +17,13 @@ int _strcmp(char *s1, char *s2)
 	for (x = 0; s1[x] != '\0'; x++)
 	{
 		check = s1[x] - s2[x];
-		if (check == '0')
+		if (check == 0)
 			continue;
+		else if (s1[x] == '\0' && s2[x] == ' ')
+		{
+			check = s1[x] - ' ';
+			break;
+		}
 		else
 			break;
 	}
