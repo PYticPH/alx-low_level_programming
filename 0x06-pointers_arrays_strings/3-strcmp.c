2243a4ae458244f0ba65dcfe37669b5c61c9fc13
PYticPH <pyticph@gmail.com>
2022-09-24 08:22:28 -0700

    Solved:  task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+int _strlen(char *c);
+/**
+ * _strcmp - Function to compare two strings
+ *
+ * @s1: parameter
+ * @s2: parameter
+ *
+ * Return: char
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int x, check;
+
+	for (x = 0; s1[x] != '\0'; x++)
+	{
+		check = s1[x] - s2[x];
+		if (check == '0')
+			continue;
+		else
+			break;
+	}
+	return  (check);
+}
