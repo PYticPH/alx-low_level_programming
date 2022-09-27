74a933c01b7378667757b42cc6ad78c478f75dc0
PYticPH <pyticph@gmail.com>
2022-09-27 07:29:11 -0700

    Solved: task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - Function to copy n bytes from memory
+ *
+ * @dest: parmeter
+ * @src: parameter
+ *
+ * Return: char
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int index;
+	int n_bytes =  n * sizeof(src[0]);
+
+	for (index = 0; index != n_bytes; index++)
+		dest[index] = src[index];
+
+	return (dest);
+}
