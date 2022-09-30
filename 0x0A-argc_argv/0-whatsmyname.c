72359cc9f0fba36cc710a701cf788250062e2bf9
PYticPH <pyticph@gmail.com>
2022-09-30 15:24:30 -0700

    Fix: task 0 #2

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,8 +8,8 @@
  * Return: SUCCESS
  */
 
-int print_argv(int argc __attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
-	return (0)
+	return (0);
 }
