d190bb19285f6180a94a833152c9dfa1a1aea940
PYticPH <pyticph@gmail.com>
2022-10-05 08:14:14 -0700

    Solved: task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+
+int _strlen(char *c);
+/**
+ * _strdup - Function to duplicate a string
+ *
+ * @str: parameter string to duplicate
+ *
+ * Return: char
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int i;
+	unsigned int len = _strlen(str);
+	char *p = malloc(sizeof(str[0]) * len);
+
+	if (str == NULL && p == NULL)
+		return ('\0');
+	for (i = 0; i <= len; i++)
+	{
+		p[i] = str[i];
+	}
+	return (p);
+}
+
+/**
+ * _strlen - Function to count lenght of string
+ *
+ * @c: parameter string to count
+ *
+ * Return: int
+ */
+
+int _strlen(char *c)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; c[i] != '\0'; i++)
+		count += 1;
+	return (count - 1);
+}
+
