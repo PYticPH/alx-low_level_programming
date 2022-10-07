38eba9fc1d8aa768e6c8b3f59881dd8dd12f25b6
PYticPH <pyticph@gmail.com>
2022-10-07 13:10:07 -0700

    Solved:  task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - Function to add two numbers
+ *
+ * @a: parameter to add
+ * @b: parameter to add
+ *
+ * Return: int
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
