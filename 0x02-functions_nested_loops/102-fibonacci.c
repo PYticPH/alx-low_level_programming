1bb0e74c31fe74f8e05fb082dc78cb13d9619cd4
PYticPH <pyticph@gmail.com>
2022-10-08 11:14:07 -0700

    Solved: task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: program to print the first 50 fibonacci sequence
+ *
+ * Return: void
+ */
+
+void main(void)
+{
+	int i;
+	long tmp_1 = 0;
+	long tmp_2 = 1;
+	long tmp_3;
+
+	for (i = 0; i <= 50; i++)
+	{
+		tmp_3 = tmp_1 + tmp_2;
+		tmp_1 = tmp_2;
+		tmp_2 = tmp_3;
+		printf("%lu, ", tmp_3);
+	}
+	printf("%lu", tmp_3);
+	printf("\n");
+}
