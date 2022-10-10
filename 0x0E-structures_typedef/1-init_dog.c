5386a1c529db13f4dd6c8a78dfa6c2790f5b0975
PYticPH <pyticph@gmail.com>
2022-10-10 10:42:19 -0700

    Solved: task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * init_dog - Function to initialize a variable type of struct dog
+ * @d: pointer to struct dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner name
+ *
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
--- 0x0E-structures_typedef/main.h
+++
@@ -1,4 +0,0 @@
-/**
- * Filetype: header file
- */
-
