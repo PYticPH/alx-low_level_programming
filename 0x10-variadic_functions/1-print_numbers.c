9e62cd3cc1cc339303f1011bc50b09c570eb23c5
PYticPH <pyticph@gmail.com>
2022-10-14 10:31:59 -0700

    Solved: task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - Variadic function to print numbers
+ *
+ * @separator: number seperator
+ * @n: number of arguments
+ *
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, int));
+		if (separator != NULL)
+		{
+			if (i != (n - 1))
+				printf("%c ", *separator);
+			else
+				printf(" ");
+		}
+	}
+	printf("\n");
+	va_end(ap);
+}
+
