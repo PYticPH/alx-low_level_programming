3119960a8eb6dcae614bdfb5484d4cb7c1fb2e7d
PYticPH <pyticph@gmail.com>
2022-10-19 02:41:56 -0700

    Fix: incorrect output #2

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,21 +12,16 @@
 int main(void)
 {
 	int i;
-	unsigned long int tmp_1 = 0;
-	unsigned long int tmp_2 = 1;
-	unsigned long int tmp_3;
+	unsigned long tmp_1 = 0;
+	unsigned long tmp_2 = 1;
+	unsigned long tmp_3;
 
 	for (i = 1; i <= 98; i++)
 	{
 		tmp_3 = tmp_1 + tmp_2;
 		tmp_1 = tmp_2;
 		tmp_2 = tmp_3;
-		if (tmp_3 > 999999999)
-		{
-			printf("%lu%lu", tmp_3 / 1000000000, tmp_3 % 1000000000);
-		}
-		else
-			printf("%lu", tmp_3);
+		printf("%lu", tmp_3);
 		if (i != 98)
 			printf(", ");
 	}
