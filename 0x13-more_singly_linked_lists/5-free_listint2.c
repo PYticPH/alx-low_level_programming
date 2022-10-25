dee7ae8047520a0f3398887605ec747cf9a29f00
PYticPH <pyticph@gmail.com>
2022-10-25 10:23:47 -0700

    Fix: incorrect output

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,13 +13,15 @@ void  free_listint2(listint_t **head)
 	listint_t *ptr;
 	listint_t *ptr2;
 
-	ptr = *head;
-
-	while (ptr != NULL)
+	if (!(*head == NULL))
 	{
-		ptr2 = ptr;
-		ptr = ptr->next;
-		free(ptr2);
+		ptr = *head;
+		while (ptr != NULL)
+		{
+			ptr2 = ptr;
+			ptr = ptr->next;
+			free(ptr2);
+		}
 	}
 	*head = NULL;
 }
