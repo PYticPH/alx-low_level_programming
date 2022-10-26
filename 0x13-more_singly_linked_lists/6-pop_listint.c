f53638d7791ca5259c25d7d0ddd5f59c2281881b
PYticPH <pyticph@gmail.com>
2022-10-26 07:00:47 -0700

    Solved: task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Function to delete the head node
+ *
+ * @head: double pointer parameter to head of list
+ *
+ * Return: int
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *ptr;
+
+	if (*head == NULL)
+		return (0);
+
+	ptr = *head;
+	*head = ptr->next;
+	free(ptr);
+	return (ptr->n);
+}
