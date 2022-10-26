dd5ce0d02aa8ef47c2962961e7285f092a1534b8
PYticPH <pyticph@gmail.com>
2022-10-26 14:00:41 -0700

    Fix: incorrect output

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,22 +11,16 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *node;
 	unsigned int limNum = 0;
 
 	if (head == NULL)
 		return (NULL);
-	if (index == 0)
-		return (head);
-
-	node = head;
-
 	while (limNum < index)
 	{
-		node = node->next;
+		head = head->next;
 		limNum++;
 	}
 
-	return (node);
+	return (head);
 }
 
