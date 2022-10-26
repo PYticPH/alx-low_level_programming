1c9c8a8729ff6dd8825a071065c89373b87d23ad
PYticPH <pyticph@gmail.com>
2022-10-26 10:37:44 -0700

    Fix: incorrect output #4

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -16,9 +16,12 @@ int pop_listint(listint_t **head)
 	if (head == NULL)
 		return (0);
 
+	if ((*head)->next == NULL)
+		return (0);
+
 	ptr = *head;
 	n = ptr->n;
-	*head = ptr->next;
+	*head = (*head)->next;
 
 	free(ptr);
 	ptr = NULL;
