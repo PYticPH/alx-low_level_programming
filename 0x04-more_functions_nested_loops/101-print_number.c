9fdef756708fa71d8fbb9f6715630864189b4e27
PYticPH <pyticph@gmail.com>
2022-10-30 13:29:20 -0700

    Fix: incorrect output

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -15,11 +15,10 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-
 		n = -n;
 	}
 
 	if (n / 10)
-		print_number(n / 10);
+		print_number((n - n % 10) / 10);
 	_putchar((n % 10) + '0');
 }
