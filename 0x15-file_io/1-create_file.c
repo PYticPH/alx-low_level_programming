a5266f39658c79f982bb3648d9cb2ddf47b57ba2
PYticPH <pyticph@gmail.com>
2022-11-01 02:47:27 -0700

    Fix: incorrect output #4

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,19 +11,20 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, dataBytes, contentBytes;
+	int fd, contentBytes;
+	int dataBytes = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	dataBytes = _strlen(text_content);
 	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 	if (fd == -1)
 		return (-1);
 
 	if (text_content != NULL)
-		contentBytes = write(fd, text_content, dataBytes);
-
+		dataBytes = _strlen(text_content);
+	
+	contentBytes = write(fd, text_content, dataBytes);
 	if (contentBytes == -1)
 		return (-1);
 
