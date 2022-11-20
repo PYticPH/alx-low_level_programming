3d8ef355818abb4ab38cbd9418c6a6b5ec89cb2f
PYticPH <pyticph@gmail.com>
2022-11-20 06:44:51 -0800

    Solved: task 10

---
+++ 0x05-pointers_arrays_strings/100.atoi.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _atoi - Function to convert string to integer
+ *
+ * @s: pointer to string parameter
+ *
+ * Return: int
+ */
+
+int _atoi(char *s)
+{
+	int index, sign = 1;
+	int intNum = 0;
+
+	for (index = 0; s[index] != '\0'; index++)
+	{
+		if (s[index] == '-')
+			sign *= -1;
+		else if (s[index] >= '0' & s[index] <= '9')
+		{
+			intNum = intNum * 10 + (s[index] - '0');
+		}
+		else if (intNum > 0)
+			break;
+	}
+
+	return (sign * intNum);
+}
+
