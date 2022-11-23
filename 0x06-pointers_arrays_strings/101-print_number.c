335aa867171e8219ccdc288641e60bd3f609acfc
PYticPH <pyticph@gmail.com>
2022-11-23 07:11:38 -0800

    Fix: output error

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,14 +10,16 @@
 
 void  print_number(int n)
 {
-	if (n < 0)
+	unsigned int intVal = n;
+
+	if (intVal < 0)
 	{
 		_putchar('-');
-		n = -n;
+		intVal = -intVal;
 	}
 
-	if (n / 10 > 0)
-		print_number(n / 10);
+	if (intVal / 10 > 0)
+		print_number(intVal / 10);
 
-	_putchar((n % 10) + '0');
+	_putchar((intVal % 10) + '0');
 }
