bf2798335ff37b8b4d716f2c58cf32e51f712d5a
PYticPH <pyticph@gmail.com>
2022-12-09 02:34:02 -0800

    Solved: task 5

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - Function to return node at index
+ *
+ * Description: return node at certain index
+ *
+ * @head: pointer to head of list
+ * @n: integer parameter of list index to return
+ *
+ * Return: dlistint_t
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int n)
+{
+	while (n)
+	{
+		head = head->next;
+		n--;
+	}
+	return (head);
+}
