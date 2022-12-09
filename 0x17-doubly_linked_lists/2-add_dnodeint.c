b802a128de2b99fd1f3df9e526fe5a0cf663159d
PYticPH <pyticph@gmail.com>
2022-12-08 21:59:46 -0800

    Fix: output error #2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -18,8 +18,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	newNode = malloc(sizeof(dlistint_t *));
 	if (newNode == NULL)
 	{
-		printf("Error: Can't malloc\n");
-		exit(EXIT_FAILURE);
+		return (NULL);
 	}
 
 	newNode->prev = NULL;
