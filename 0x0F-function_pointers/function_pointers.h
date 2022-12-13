7bb8ec1c8d6f6ffdb9a73bb71e5da001d26fd21e
PYticPH <pyticph@gmail.com>
2022-12-13 13:08:54 -0800

    Solved: task 0

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,13 @@
+/**
+ * Filetype - c header file
+ */
+
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+#include <stdio.h>
+
+int _putchar(char);
+void print_name(char *name, void (*f) (char *));
+
+#endif
