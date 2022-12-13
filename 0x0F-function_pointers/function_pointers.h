672da05af0dc34d7126a7854ec9a014057cb2bdd
PYticPH <pyticph@gmail.com>
2022-12-13 13:49:43 -0800

    Fix: output error #3

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,12 +1,13 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+
 #include <stdio.h>
 
 /**
  * Filetype - C header file
  */
 
-int _putchar(char);
+int _putchar(char c);
 void print_name(char *name, void (*f) (char *));
 
 #endif
