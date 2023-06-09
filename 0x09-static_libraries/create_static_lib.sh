d6612e41d5934f5148d92860c3691698c7321ab5
PYticPH <pyticph@gmail.com>
2023-06-09 16:54:33 -0400

    Fix: fix library name

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-#script to create custom library
+#script to create static library liball.o
 
-gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c "*.c"
-ar -rc libmy.a "*.o"
+echo "Compiling source code to object code..."
+gcc -Wall -Wexral -Werror -pedantic -std=gnu89 -c "*.c"
+echo "Done: object code created successfully"
+echo "Creating library..."
+ar -rc liball.a "*.o"
+echo "Done: library created successfully"
