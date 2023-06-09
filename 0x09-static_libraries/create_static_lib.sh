56a605c295c3efb50dd94960f5440631a4896bb7
PYticPH <pyticph@gmail.com>
2023-06-09 16:13:34 -0400

    Fix: fix globbing

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -3,10 +3,10 @@
 
 #compile files to object files
 echo "Compiling C files into object files"
-gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c *.c
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c "*.c"
 echo "Compilation done: object files created successfully"
 
 #create custom library
 echo "Creating custom library"
-ar -rc libmy.a *.o
+ar -rc libmy.a "*.o"
 echo "Done: library created successfully"
