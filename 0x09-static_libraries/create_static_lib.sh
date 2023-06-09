a8be4371d4b5a1edf86b5a2ab0ddb11e64f115f3
PYticPH <pyticph@gmail.com>
2023-06-09 16:29:19 -0400

    Fix: script clean up

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 # script to create custom library
-
-#compile files to object files
-echo "Compiling C files into object files"
 gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c "*.c"
-echo "Compilation done: object files created successfully"
-
-#create custom library
-echo "Creating custom library"
 ar -rc libmy.a "*.o"
-echo "Done: library created successfully"
