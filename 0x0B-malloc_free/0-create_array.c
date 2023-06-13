4b8f35c2d795f35d588b03d39bad5bb4e741183b
PYticPH <pyticph@gmail.com>
2023-06-13 23:34:53 +0100

    Solved: task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_array - Function to create array
+ *
+ * @size: size of array
+ * @c: array characters/elements
+ *
+ * Return: success or failure
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i = 0;
+
+	ptr = malloc((sizeof(char) * size) + 1);
+
+	if (!ptr)
+		return (NULL);
+
+	while (i < size)
+	{
+		ptr[i] = c;
+		i++;
+	}
+
+	return (ptr);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Filetype - header file
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char);
+char *create_array(unsigned int, char);
+
+#endif
