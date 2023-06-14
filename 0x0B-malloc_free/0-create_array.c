62e42b34d26c27ef6424c78aaf622ba0423b7390
PYticPH <pyticph@gmail.com>
2023-06-14 17:46:31 +0100

    Fix: allocate required memory with malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 	unsigned int i = 0;
 
-	ptr = malloc(sizeof(char) * size);
+	ptr = malloc(sizeof(char) * size + 1);
 
 	if (ptr == NULL || size == 0)
 		return (NULL);
