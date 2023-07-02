a096fe23b559f92e588aadfae4eb3f5867225106
PYticPH <76103068+PYticPH@users.noreply.github.com>
2023-07-02 22:06:02 +0100

    Delete 0x09-static_libraries directory

--- 0x09-static_libraries/create_static_lib.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#script to create static library liball.o
-
-echo "Compiling source code to object code..."
-gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c *.c
-echo "Done: object code created successfully"
-echo "Creating library..."
-ar -rc liball.a *.o
-echo "Done: library created successfully"
