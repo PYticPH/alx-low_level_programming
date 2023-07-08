ff384bd403992063c7895ecc86dfd9ec48b8b80e
PYticPH <pyticph@gmail.com>
2023-07-08 01:37:05 +0100

    Solved: task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* @argc: number of argument
+* @argv: array of argument
+*
+* Return: void
+*/
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
